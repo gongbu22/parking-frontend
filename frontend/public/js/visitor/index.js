ef2721f02b50f98a0f4edc47cda5cd00137b94b4
gongbu22 <pyujin0711@naver.com>
2024-10-24 23:05:28 +0900

    구조, 차량 등록, 남은 주차 자리수

---
+++ frontend/public/js/visitor/index.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', async () => {
+    try {
+        remainCar();
+    } catch (e) {
+        console.log(e);
+        alert('남은 주차 자리수 조회 실패!');
+    }
+})
+
+// 남은 주차자리 수
+const remainCar = async () => {
+    // let url = 'http://127.0.0.1:8000/remainCar'
+    // const res = await fetch(url);
+    // if (res.ok) {
+    //     const data = await res.json();
+        const remainCars = document.querySelector('#remainCars');
+        const remainBarrier = document.querySelector('#remainBarrier');
+
+        // let html = `${data.remainCars}/100`;
+        let html1 = `50/100`;
+        remainCars.innerHTML = html1;
+
+        // let html2 = `${data.remainBarrier}/2`;
+        let html2 = `1/2`;
+        remainBarrier.innerHTML = html2;
+    // }
+}
+
+// 차량 등록
+const regbtn = document.querySelector('#regbtn');
+const carnumfrm = document.forms['carnumfrm'];
+
+regbtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const formData = new FormData(carnumfrm);
+
+    let jsondata = {};
+    formData.forEach((val, key) => {
+        jsondata[key] = val;
+    });
+    console.log(jsondata);
+
+    // const res = await fetch('http://127.0.0.1:8000/carregist',
+    //     {
+    //         method: 'POST',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(jsondata)
+    //     })
+    //     .then(res => {
+    //         alert('등록되었습니다.');
+    //     }).catch((error) => {
+    //         alert('등록 실패 :(');
+    //     });
+
+})
